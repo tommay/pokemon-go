41fb2e7853fff56105ae64d87a11eb94b93e518c
Tom May <tom@tommay.net>
2017-11-03 17:56:38 -0700

    Add level -c CP to show levels which could have the CP.

--- app/Level.hs
+++ app/Level.hs
@@ -8,17 +8,24 @@ import qualified Calc
 import qualified Epic
 import qualified GameMaster
 import           GameMaster (GameMaster)
+import           PokemonBase (PokemonBase)
 
 import qualified System.IO as I
 import qualified Text.Printf as Printf
 
 data Options = Options {
+  cp      :: Maybe Int,
   species :: String
 }
 
 getOptions :: IO Options
 getOptions = do
-  let opts = Options <$> optSpecies
+  let opts = Options <$> optCp <*> optSpecies
+      optCp = O.optional $ O.option O.auto
+        (  O.long "cp"
+        <> O.short 'c'
+        <> O.metavar "CP"
+        <> O.help "Print possible levels for the given CP")
       optSpecies = O.argument O.str (O.metavar "SPECIES")
       options = O.info (opts <**> O.helper)
         (  O.fullDesc
@@ -38,12 +45,34 @@ main = Epic.catch (
     pokemonBase <- GameMaster.getPokemonBase gameMaster $
       species options
 
-    let levels = [1..30]   -- maxEncounterPlayerLevel: 30, max catch level.
-    mapM_ (\level -> do
-            let cpMultiplier = GameMaster.getCpMultiplier gameMaster level
-                min = Calc.cp pokemonBase cpMultiplier 0 0 0
-                max = Calc.cp pokemonBase cpMultiplier 15 15 15
-            putStrLn $ Printf.printf "%2d: %4d %4d" ((floor level) :: Int) min max)
-      levels
+    case cp options of
+      Nothing -> printAllLevels gameMaster pokemonBase
+      Just cp -> printLevelsForCp gameMaster pokemonBase cp
   )
   $ \ex -> I.hPutStrLn I.stderr $ ex
+
+levels = [1..30]   -- maxEncounterPlayerLevel: 30, max catch level.
+
+printAllLevels :: GameMaster -> PokemonBase -> IO ()
+printAllLevels gameMaster pokemonBase = do
+  mapM_ (\level -> do
+          let (min, max) = cpMinMax gameMaster pokemonBase level
+          putStrLn $ Printf.printf "%2d: %4d %4d" level min max)
+    levels
+
+printLevelsForCp :: GameMaster -> PokemonBase -> Int -> IO ()
+printLevelsForCp gameMaster pokemonBase cp = do
+  let levels' = filter (\level ->
+          let (min, max) = cpMinMax gameMaster pokemonBase level
+          in min <= cp && cp <= max)
+        levels
+      min = minimum levels'
+      max = maximum levels'
+   in putStrLn $ show min ++ " - " ++ show max
+
+cpMinMax :: GameMaster -> PokemonBase -> Int -> (Int, Int)
+cpMinMax gameMaster pokemonBase level =
+  let cpMultiplier = GameMaster.getCpMultiplier gameMaster (fromIntegral level)
+      min = Calc.cp pokemonBase cpMultiplier 0 0 0
+      max = Calc.cp pokemonBase cpMultiplier 15 15 15
+  in (min, max)
