00f02c617777bea67850b050f90e64cfdb948470
Tom May <tom@tommay.net>
2017-12-10 01:59:47 -0800

    Add TM.hs to show which matchups get worse when a move is TMs.  This would actually be a lot easier with mysql.

---
+++ app/TM.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import qualified Options.Applicative as O
+import           Options.Applicative ((<|>), (<**>))
+import           Data.Semigroup ((<>))
+
+import qualified Epic
+import qualified GameMaster
+import           GameMaster (GameMaster)
+import qualified Matchup
+import           Matchup (Matchup)
+import qualified Util
+
+import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Maybe as Maybe
+import qualified System.IO as IO
+import qualified Text.Printf as Printf
+
+data Options = Options {
+  species :: String,
+  old :: String,
+  new :: String
+}
+
+getOptions :: IO Options
+getOptions =
+  let opts = Options <$> optSpecies <*> optOld <*> optNew
+      optSpecies = O.argument O.str (O.metavar "SPECIES")
+      optOld = O.argument O.str (O.metavar "OLDMOVE")
+      optNew = O.argument O.str (O.metavar "NEWMOVE")
+      options = O.info (opts <**> O.helper)
+        (  O.fullDesc
+        <> O.progDesc "Show which matchups get worse if a move is TMd.")
+      prefs = O.prefs O.showHelpOnEmpty
+  in O.customExecParser prefs options
+
+main =
+  Epic.catch (
+    do
+      options <- getOptions
+
+      gameMaster <- do
+        ioGameMaster <- GameMaster.load "GAME_MASTER.yaml"
+        ioGameMaster
+
+      matchups <- do
+        ioMatchups <- Matchup.load "matchups.out"
+        ioMatchups
+
+      let worse = getWorseMatchups gameMaster matchups
+            (species options) (old options) (new options)
+
+      mapM_ (putStrLn . showWorse) worse
+    )
+    $ \ex -> IO.hPutStrLn IO.stderr ex
+
+showWorse :: (String, String, Int, Int) -> String
+showWorse (defender, charge, oldDamage, newDamage) =
+  Printf.printf "%s %s: %d => %d" defender charge oldDamage newDamage
+
+getWorseMatchups :: GameMaster -> [Matchup] -> String -> String -> String ->
+  [(String, String, Int, Int)]
+getWorseMatchups gameMaster matchups species old new =
+  let mOld = filter ((== [species, old]) . makeFilterKey) matchups
+      mNew = Util.groupBy makeGroupKey $
+        filter ((== [species, new]) . makeFilterKey) matchups
+  in concat $ map (\ old ->
+       case HashMap.lookup (makeGroupKey old) mNew of
+         Nothing -> []
+         Just news -> 
+           Maybe.mapMaybe (\ new ->
+             if Matchup.minDamage new < Matchup.minDamage old
+               then Just (Matchup.defender old, Matchup.charge old,
+                 Matchup.minDamage old, Matchup.minDamage new)
+               else Nothing) news)
+       mOld
+
+makeFilterKey :: Matchup -> [String]
+makeFilterKey =
+  sequence [Matchup.attacker, Matchup.quick]
+
+makeGroupKey :: Matchup -> [String]
+makeGroupKey =
+  sequence [Matchup.defender, Matchup.charge]
+
