0a4d42afb976d1772f1a2b1fa6898386c23e9ecb
Tom May <tom@tommay.net>
2017-12-11 05:11:20 -0800

    level -2 uses an Int -> Bool predicate instead of a Range.

--- app/Level.hs
+++ app/Level.hs
@@ -10,22 +10,20 @@ import qualified GameMaster
 import           GameMaster (GameMaster)
 import           PokemonBase (PokemonBase)
 
-import qualified Data.Range.Range as Range
-import           Data.Range.Range (Range (SpanRange))
 import qualified System.IO as I
 import qualified Text.Printf as Printf
 
 data Options = Options {
-  ivRange :: Range Int,
+  ivPred  :: Int -> Bool,
   cp      :: Maybe Int,
   species :: String
 }
 
 getOptions :: IO Options
 getOptions =
-  let opts = Options <$> optIvRange <*> optCp <*> optSpecies
-      optIvRange = optTwo <|> pure (SpanRange 0 45)
-      optTwo = O.flag' (SpanRange 30 45)
+  let opts = Options <$> optIvPred <*> optCp <*> optSpecies
+      optIvPred = optTwo <|> pure (const True)
+      optTwo = O.flag' (>= 30)
         (  O.long "two"
         <> O.short '2'
         <> O.help "Show levels for IVs that are at least \"strong\"")
@@ -53,37 +51,37 @@ main = Epic.catch (
     pokemonBase <- GameMaster.getPokemonBase gameMaster $
       species options
 
-    let ivRange = Main.ivRange options
+    let ivPred = Main.ivPred options
 
     case cp options of
-      Nothing -> printAllLevels gameMaster pokemonBase ivRange
-      Just cp -> printLevelsForCp gameMaster pokemonBase ivRange cp
+      Nothing -> printAllLevels gameMaster pokemonBase ivPred
+      Just cp -> printLevelsForCp gameMaster pokemonBase ivPred cp
   )
   $ \ex -> I.hPutStrLn I.stderr $ ex
 
 levels = [1..40]  -- Catches can npw exceed maxEncounterPlayerLevel (30)
                   -- depending on the weather.
 
-printAllLevels :: GameMaster -> PokemonBase -> Range Int -> IO ()
-printAllLevels gameMaster pokemonBase ivRange = do
+printAllLevels :: GameMaster -> PokemonBase -> (Int -> Bool) -> IO ()
+printAllLevels gameMaster pokemonBase ivPred = do
   mapM_ (\level -> do
-          let (min, max) = cpMinMax gameMaster pokemonBase ivRange level
+          let (min, max) = cpMinMax gameMaster pokemonBase ivPred level
           putStrLn $ Printf.printf "%2d: %4d %4d" level min max)
     levels
 
-printLevelsForCp :: GameMaster -> PokemonBase -> Range Int -> Int -> IO ()
-printLevelsForCp gameMaster pokemonBase ivRange cp = do
+printLevelsForCp :: GameMaster -> PokemonBase -> (Int -> Bool) -> Int -> IO ()
+printLevelsForCp gameMaster pokemonBase ivPred cp = do
   let levels' = filter (\level ->
-          let (min, max) = cpMinMax gameMaster pokemonBase ivRange level
+          let (min, max) = cpMinMax gameMaster pokemonBase ivPred level
           in min <= cp && cp <= max)
         levels
       min = minimum levels'
       max = maximum levels'
    in putStrLn $ show min ++ " - " ++ show max
 
-cpMinMax :: GameMaster -> PokemonBase -> Range Int -> Int -> (Int, Int)
-cpMinMax gameMaster pokemonBase ivRange level =
-  let ivs = possibleIvs ivRange
+cpMinMax :: GameMaster -> PokemonBase -> (Int -> Bool) -> Int -> (Int, Int)
+cpMinMax gameMaster pokemonBase ivPred level =
+  let ivs = possibleIvs ivPred
       cpMultiplier = GameMaster.getCpMultiplier gameMaster (fromIntegral level)
       cp (attack, defense, stamina) =
         Calc.cp pokemonBase cpMultiplier attack defense stamina
@@ -92,9 +90,9 @@ cpMinMax gameMaster pokemonBase ivRange level =
       max = maximum cps
   in (min, max)
 
-possibleIvs :: Range Int -> [(Int, Int, Int)]
-possibleIvs ivRange =
+possibleIvs :: (Int -> Bool) -> [(Int, Int, Int)]
+possibleIvs ivPred =
   [(attack, defense, stamina) |
     let ivs = [0 .. 15],
     attack <- ivs, defense <- ivs, stamina <- ivs,
-    Range.inRange ivRange (attack + defense + stamina)]
+    ivPred (attack + defense + stamina)]
