d5a98d7e4da4b3aada8ed534648d9f4fc1585b20
Tom May <tom@tommay.net>
2017-12-20 18:09:13 -0800

    Resurrect and rework Battler.hs to see battle logs.

--- app/Battler.hs
+++ app/Battler.hs
@@ -1,16 +1,3 @@
--- What do I want to know?  The defender always has an unknown moveset
--- so the answers will always come from AttackerResult which takes
--- the worst case over all defender movesets.
---
--- What are the most effective species/moveset attackers?
---   Sort [AttackerResult] by minDamage and take the top N.
--- What are the top attackers regardless of moveset?
-
--- For a given attacker, what is it most effective against?
--- For a given attacker/movsset, what is it most effective against?
--- How "good" is a given attacker apecies?
--- How "good" is a given attacker apecies with a given movset?
-
 {-# LANGUAGE OverloadedStrings #-}
 -- {-# LANGUAGE DuplicateRecordFields #-}
 -- {-# LANGUAGE DisambiguateRecordFields #-}
@@ -22,6 +9,7 @@ import           Options.Applicative ((<|>), (<**>))
 import           Data.Semigroup ((<>))
 
 import qualified Action
+import           Action (Action)
 import qualified Attacker
 import           Attacker (Attacker)
 import qualified Battle
@@ -41,31 +29,35 @@ import qualified Pokemon
 import           Pokemon (Pokemon)
 import qualified PokemonBase
 import           PokemonBase (PokemonBase)
-import qualified Type
-import           Type (Type)
-import qualified Util
+import           Weather (Weather (..))
 
-import           Control.Applicative (optional, some)
 import           Control.Monad (join)
 import qualified Control.Monad.Writer as Writer
---import qualified Data.Attoparsec.Text as AP
+import           Control.Monad.Writer (Writer)
 import qualified Data.List as List
-import qualified Data.Maybe as Maybe
---import qualified Data.Set as Set
---import qualified Data.Text as Text
 import qualified System.IO as I
 import qualified Text.Printf as Printf
---import qualified Text.Regex as Regex
 
 defaultLevel = 20
 
 data Options = Options {
+  maybeWeather :: Maybe Weather,
+  attacker :: String,
   defender :: String
 }
 
 getOptions :: IO Options
 getOptions =
-  let opts = Options <$> optDefender
+  let opts = Options <$> optWeather <*> optAttacker <*> optDefender
+      optWeather = O.optional $
+            O.flag' Clear (O.long "clear" <> O.help "weather is sunny/clear")
+        <|> O.flag' Fog (O.long "fog")
+        <|> O.flag' Overcast (O.long "overcast")
+        <|> O.flag' PartlyCloudy (O.long "partlycloudy")
+        <|> O.flag' Rainy (O.long "rainy")
+        <|> O.flag' Snow (O.long "snow")
+        <|> O.flag' Windy (O.long "windy")
+      optAttacker = O.argument O.str (O.metavar "Attacker")
       optDefender = O.argument O.str (O.metavar "DEFENDER")
       options = O.info (opts <**> O.helper)
         (  O.fullDesc
@@ -80,65 +72,42 @@ main =
 
       gameMaster <- join $ GameMaster.load "GAME_MASTER.yaml"
 
-      defenders <- makeWithAllMovesetsFromSpecies gameMaster (defender options)
+      let weatherBonus = case maybeWeather options of
+            Just weather -> GameMaster.getWeatherBonus gameMaster weather
+            Nothing -> const 1
 
-      attackers <- do
-        mythicalMap <- join $ Mythical.load "mythical.yaml"
-        let notMythical =
-              not . Mythical.isMythical mythicalMap . PokemonBase.species
-            notLegendary =
-              not . Mythical.isLegendary mythicalMap . PokemonBase.species
-            allBases = GameMaster.allPokemonBases gameMaster
-            attackerBases =
-                filter notMythical
-              $ filter notLegendary
-              $ filter (not . PokemonBase.hasEvolutions)
-              allBases
-            attackers = concat $ map
-              (makeWithAllMovesetsFromBase gameMaster defaultLevel)
-              attackerBases
-        return attackers
+      attackerVariants <-
+        makeWithAllMovesetsFromSpecies gameMaster (attacker options)
 
-      let attackerResults =
-            [getAttackerResult defenders attacker | attacker <- attackers]
+      defenderVariants <-
+        makeWithAllMovesetsFromSpecies gameMaster (defender options)
 
-      let highDps = keepHighDpsSpecies attackerResults
+      let battleWriters =
+            [Battle.runBattle $ Battle.init weatherBonus attacker defender |
+              attacker <- attackerVariants, defender <- defenderVariants]
+          battleResults = map Writer.runWriter battleWriters
 
-      let byDamage = reverse $ Util.sortWith minDamage highDps
-
-      mapM_ (putStrLn . showAttackerResult) byDamage
+      putStrLn $ List.intercalate "\n" $ map showBattle battleResults
     )
     $ I.hPutStrLn I.stderr
 
-keepHighDpsSpecies :: [AttackerResult] -> [AttackerResult]
-keepHighDpsSpecies attackerResults =
-  let sortedByDps = reverse $ Util.sortWith dps attackerResults
-      highDps = take (length sortedByDps `div` 6) sortedByDps
-      resultSpecies = Pokemon.species . pokemon
-      keepSpecies = List.nub $ List.sort $ map resultSpecies highDps
-  in filter (\ result -> resultSpecies result `elem` keepSpecies)
-       attackerResults
+tell :: a -> Writer [a] ()
+tell a =
+  Writer.tell [a]
 
-{-
-gyarados quick/charge defends against
-  order by damage inflicted
-  pidgey quick/charge dps damage inflicted
-  pidgey quick/charge dps damage inflicted
-  caterpie quick/charge dps damage inflicted
-  caterpie quick/charge dps damage inflicted
--}
-
-showAttackerResult :: AttackerResult -> String
-showAttackerResult result =
-  let attacker = showPokemon $ pokemon $ result
-      minDamage' = minDamage result
-      dps' = dps result
-      maxDamage' = maxDamage result
-  in Printf.printf "%-35s %.1f %d - %d" attacker dps' minDamage' maxDamage'
+showBattle :: (Battle, [Action]) -> String
+showBattle (battle, actions) = 
+  List.intercalate "\n" $
+  Writer.execWriter (
+    do
+      tell $ showPokemon $ Attacker.pokemon $ Battle.attacker battle
+      tell $ showPokemon $ Defender.pokemon $ Battle.defender battle
+      mapM_ (tell . Action.what) actions)
 
 showPokemon :: Pokemon -> String
 showPokemon pokemon =
-  Printf.printf "%s %s / %s" (Pokemon.species pokemon)
+  Printf.printf "%s %s / %s"
+    (Pokemon.species pokemon)
     (Move.name $ Pokemon.quick pokemon)
     (Move.name $ Pokemon.charge pokemon)
 
@@ -167,65 +136,3 @@ makeWithAllMovesetsFromBase gameMaster level base =
   in [makeAttacker quickMove chargeMove |
       (quickMove, chargeMove) <-
         PokemonBase.moveSets base]
-
--- Results of a particular attacker/moveset against all defender
--- movesets.  The pokemon is expected to score at least minDamage no
--- matter what the defender's moveset is.
-
-data AttackerResult = AttackerResult {
-  pokemon :: Pokemon,
-  battles :: [Battle],
-  minByDamage :: Battle,
-  minDamage :: Int,
-  dps       :: Float,
-  maxDamage :: Int
-}
-
-getAttackerResult :: [Pokemon] -> Pokemon -> AttackerResult
-getAttackerResult defenders attacker =
-  let battleWriters =
-        [Battle.runBattle $ Battle.init (const 1) attacker defender |
-        defender <- defenders]
-      battles = map (fst . Writer.runWriter) battleWriters
-      minByDamage =
-        List.minimumBy (Util.compareWith Battle.damageInflicted) battles
-      minDamage = Battle.damageInflicted minByDamage
-      maxByDamage =
-        List.maximumBy (Util.compareWith Battle.damageInflicted) battles
-      maxDamage = Battle.damageInflicted maxByDamage
-  in AttackerResult {
-       pokemon = attacker,
-       battles = battles,
-       minByDamage = minByDamage,
-       minDamage = minDamage,
-       dps = Battle.dps minByDamage,
-       maxDamage = maxDamage
-       }
-
-{-
--- Results of a particular attacker species against all defender movesets.
--- Really we want to know the moveset with the highest minDamage because
--- it's the lower bound
-
-data AttackerBaseResult = AttackerBaseResult {
-  pokemonBase :: PokemonBase,
-  attackerResults :: [AttackerResult],
-  minByDamage :: AttackerResult,
-  minDamage :: Int
-}
-
-getAttackerBaseResult :: GameMaster -> [Pokemon] -> PokemonBase -> AttackerBaseResult
-getAttackerBaseResult gameMaster defenders attackerBase =
-  let attackers =
-        makeWithAllMovesetsFromBase gameMaster defaultLevel attackerBase
-      attackerResults = [
-        getAttackerResult defenders attacker | attacker <- attackers]
-      minByDamage = List.minimumBy (Util.compareWith minDamage) attackerResults
-      minDamage = AttackerResult.minDamage minByDamage
-  in AttackerBaseResult {
-    pokemonBase = attackerBase,
-    attackerResults = attackerResults,
-    minByDamage = minByDamage,
-    minDamage = minDamage
-    }
--}
