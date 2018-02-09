ea0da60d64a03cc28c0ac1cabaaf4d36ccca0f43
Tom May <tom@tommay.net>
2018-02-09 11:37:33 -0800

    Add TweakLevel.optMaybeTweakLevel,

--- src/TweakLevel.hs
+++ src/TweakLevel.hs
@@ -1,5 +1,6 @@
 module TweakLevel (
-  optTweakLevel
+  optTweakLevel,
+  optMaybeTweakLevel,
 ) where
 
 import qualified Options.Applicative as O
@@ -7,16 +8,20 @@ import           Options.Applicative ((<|>))
 import           Data.Semigroup ((<>))
 
 optTweakLevel :: O.Parser (Float -> Float)
-optTweakLevel =
-  let optSetLevel = const <$> O.option O.auto
-        (  O.long "level"
-        <> O.short 'l'
-        <> O.metavar "LEVEL"
-        <> O.help ("Tweak my_pokemon level to find who's implicitly best, " ++
-             "or set the level for -a or the default level for -m"))
-      optMinLevel = (\l1 l2 -> maximum [l1, l2]) <$> O.option O.auto
-        (  O.long "minlevel"
-        <> O.short 'n'
-        <> O.metavar "LEVEL"
-        <> O.help "Tweak level of pokemon read from FILE to be at least LEVEL")
-  in optSetLevel <|> optMinLevel <|> pure id
+optTweakLevel = optSetLevel <|> optMinLevel <|> pure id
+
+optMaybeTweakLevel :: O.Parser (Maybe (Float -> Float))
+optMaybeTweakLevel = O.optional $ optSetLevel <|> optMinLevel
+
+optSetLevel = const <$> O.option O.auto
+  (  O.long "level"
+  <> O.short 'l'
+  <> O.metavar "LEVEL"
+  <> O.help ("Tweak my_pokemon level to find who's implicitly best, " ++
+       "or set the level for -a or the default level for -m"))
+
+optMinLevel = (\l1 l2 -> maximum [l1, l2]) <$> O.option O.auto
+  (  O.long "minlevel"
+  <> O.short 'n'
+  <> O.metavar "LEVEL"
+  <> O.help "Tweak level of pokemon read from FILE to be at least LEVEL")
