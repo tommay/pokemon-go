4c5a6c244ad2462123543eed337d9b33115fd1e9
Tom May <tom@tommay.net>
2018-02-09 11:25:48 -0800

    UnIV uses TweakLevel so it can calculate cp etc. for various levels.

--- app/UnIV.hs
+++ app/UnIV.hs
@@ -8,7 +8,7 @@ import qualified GameMaster
 import           GameMaster (GameMaster)
 import qualified MyPokemon
 import           MyPokemon (MyPokemon)
-import qualified PokemonBase (PokemonBase)
+import qualified TweakLevel
 
 import qualified Options.Applicative as O
 import           Options.Applicative ((<**>))
@@ -20,12 +20,14 @@ import qualified Data.Yaml.Builder as Builder
 import qualified System.IO as I
 
 data Options = Options {
+  maybeTweakLevel :: Maybe (Float -> Float),
   maybeFilename  :: Maybe String
 }
 
 getOptions :: IO Options
 getOptions =
-  let opts = Options <$> optFilename
+  let opts = Options <$> optMaybeTweakLevel <*> optFilename
+      optMaybeTweakLevel = TweakLevel.optMaybeTweakLevel
       optFilename = O.optional $ O.argument O.str (O.metavar "FILENAME")
       options = O.info (opts <**> O.helper)
         (  O.fullDesc
@@ -41,13 +43,15 @@ main = Epic.catch (
 
     myPokemon <- join $ MyPokemon.load $ maybeFilename options
 
-    myNewPokemon <- mapM (updateFromIVs gameMaster) myPokemon
+    myNewPokemon <-
+      mapM (updateFromIVs gameMaster (maybeTweakLevel options)) myPokemon
     B.putStr $ Builder.toByteString myNewPokemon
   )
   $ I.hPutStrLn I.stderr
 
-updateFromIVs :: (Epic.MonadCatch m) => GameMaster -> MyPokemon -> m MyPokemon
-updateFromIVs gameMaster myPokemon = do
+updateFromIVs :: (Epic.MonadCatch m) =>
+  GameMaster -> Maybe (Float -> Float) -> MyPokemon -> m MyPokemon
+updateFromIVs gameMaster maybeTweakLevel myPokemon = do
   pokemonBase <-
     GameMaster.getPokemonBase gameMaster $ MyPokemon.species myPokemon
   let failNoIVs = Epic.fail $ MyPokemon.name myPokemon ++ ": no ivs"
@@ -55,11 +59,17 @@ updateFromIVs gameMaster myPokemon = do
     Nothing -> failNoIVs
     Just [] -> failNoIVs
     Just (ivs:_) ->
-      let cp = Calc.cp gameMaster pokemonBase ivs
-          hp = Calc.hp gameMaster pokemonBase ivs
+      let ivs' = case maybeTweakLevel of
+            Nothing -> ivs
+            Just tweakLevel -> IVs.setLevel ivs $ tweakLevel $ IVs.level ivs
+          cp = Calc.cp gameMaster pokemonBase ivs'
+          hp = Calc.hp gameMaster pokemonBase ivs'
           stardust =
-            GameMaster.getStardustForLevel gameMaster $ IVs.level ivs
+            GameMaster.getStardustForLevel gameMaster $ IVs.level ivs'
       in return $ myPokemon {
+        MyPokemon.ivs = case maybeTweakLevel of
+          Nothing -> MyPokemon.ivs myPokemon
+          _ -> Just [ivs'],
         MyPokemon.cp = cp,
         MyPokemon.hp = hp,
         MyPokemon.stardust = stardust
