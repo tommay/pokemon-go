d8734cda9ad5b51ce5431ef4f7a1e5041c578ec4
Tom May <tom@tommay.net>
2018-09-01 00:01:45 -0700

    Add comments to Logger.hs.

--- src/Logger.hs
+++ src/Logger.hs
@@ -1,3 +1,8 @@
+-- Logger is the Writer monad but it hides the fact that it maintains
+-- a List of things.  I.e., instead of declaring "Writer [w] a" it's
+-- just "Logger w a", and similarly it's just "Logger.log w" instead
+-- of "Writer.tell [w]".
+
 module Logger (
   Logger,
   Logger.log,
@@ -15,10 +20,10 @@ log :: w -> Logger w ()
 log w = Writer.tell [w]
 
 runLogger :: Logger w a -> (a, [w])
-runLogger =  Writer.runWriter
+runLogger = Writer.runWriter
 
 execLogger :: Logger w a -> [w]
-execLogger =  Writer.execWriter
+execLogger = Writer.execWriter
 
 logger :: (a, [w]) -> Logger w a
 logger = Writer.writer
