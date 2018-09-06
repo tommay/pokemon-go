9abf20a4a958cd8e42e19ffff15e2d311246a2b8
Tom May <tom@tommay.net>
2018-09-06 11:14:16 -0700

    Return to an ad-hoc Apraisal parser.  It's easier.

--- src/Appraisal.hs
+++ src/Appraisal.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Appraisal (
   new,
   possibleIvs
@@ -7,82 +5,26 @@ module Appraisal (
 
 import qualified Epic
 
-import qualified Data.Attoparsec.Text as Atto
 import qualified Data.List as List
 import qualified Data.Range.Range as Range
 import           Data.Range.Range (Range (SpanRange))
-import qualified Data.Text as Text
-import           Options.Applicative ((<|>))
 import qualified Text.Regex as Regex
-
-import qualified Debug
 
 data Appraisal = Appraisal {
   summary     :: Range Int,
-  bestIvs     :: [Text.Text],
+  best        :: [String],
   bestRange   :: Range Int
 } deriving (Show)
 
 new :: (Epic.MonadCatch m) => String -> m Appraisal
-new string =
-  let parseAppraisal = do
-        summary <- parseSummary
-        comma
-        bestIvs <- parseBestIvs
-        comma
-        bestRange <- parseBestRange
-        Atto.endOfInput
-        return $ Appraisal summary bestIvs bestRange
-  in case Atto.parseOnly parseAppraisal (Text.pack string) of
-    Left error -> Epic.fail $ "Bad appraisal: " ++ getError error
-    Right appraisal -> return appraisal
-
-comma :: Atto.Parser ()
-comma = do
-  Atto.char ','
-  Atto.skipSpace
-
--- This is terrible.  I want to report decent smantic errors but Attoparsec
--- insists on prefixing all error messages with some junk.  So I start my
--- errors with errorChar and discard the prefix.
-
-errorChar :: Char
-errorChar = '='
-
-reportError :: String -> Atto.Parser a
-reportError string =
-  fail $ errorChar : string
-
-getError :: String -> String
-getError = tail . dropWhile (/= errorChar)
-
-parseSummary :: Atto.Parser (Range Int)
-parseSummary =
-  Atto.choice [
-    inList ["wonder", "amazes", "best"]      $ SpanRange 37 45,
-    inList ["attention", "strong"]           $ SpanRange 30 36,
-    inList ["above", "decent"]               $ SpanRange 23 29,
-    inList ["not likely", "may not", "room"] $ SpanRange 0 22
-  ] <|> reportError "Bad summary string"
-
-inList :: [Text.Text] -> a -> Atto.Parser a
-inList list result = do
-  Atto.choice $ map Atto.string list
-  return result
-
-parseBestIvs :: Atto.Parser [Text.Text]
-parseBestIvs = do
-  bestIvs <- Atto.sepBy1 ("attack" <|> "defense" <|> "hp") comma
-  return $ List.sort $ List.nub bestIvs
-
-parseBestRange :: Atto.Parser (Range Int)
-parseBestRange =
-  Atto.choice [
-    inList ["not out", "point", "basic"]            $ SpanRange 0 7,
-    inList ["trending", "indicate", "definitely"]   $ SpanRange 8 12,
-    inList ["impressed", "excellent", "impressive"] $ SpanRange 13 14,
-    inList ["incredible", "blown", "no doubt"]      $ SpanRange 15 15
-  ]
+new string = do
+  let words = split " *, *" string
+  (summary', words) <- getSummary words
+  (best', words) <- getBest words
+  (bestRange', words) <- getBestRange words
+  case words of
+    [] -> return $ Appraisal summary' best' bestRange'
+    _ -> Epic.fail $ "junk at the end of appraisal"
 
 possibleIvs :: Appraisal -> [(Int, Int, Int)]
 possibleIvs this =
@@ -91,23 +33,68 @@ possibleIvs this =
     attack <- ivs, defense <- ivs, stamina <- ivs,
     ok this attack defense stamina]
 
+getSummary :: (Epic.MonadCatch m) => [String] -> m (Range Int, [String])
+getSummary words =
+  let
+    getRange phrase
+      | phrase `elem` ["not likely", "may not", "room"] = range 0 22
+      | phrase `elem` ["above", "decent", "decent"]     = range 23 29
+      | phrase `elem` ["attention", "strong", "strong"] = range 30 36
+      | phrase `elem` ["wonder", "amazes", "best"]      = range 37 45
+      | otherwise = Epic.fail $ "bad summary in appraisal"
+  in case words of
+    summary : rest -> do
+      range <- getRange summary
+      return $ (range, rest)
+    _ -> Epic.fail $ "missing summary in appraisal"
+
+range :: (Monad m) => Int -> Int -> m (Range Int)
+range low high =
+  return $ SpanRange low high
+
+getBest :: (Epic.MonadCatch m) => [String] -> m ([String], [String])
+getBest words =
+  let (best, rest) = span (`elem` ["attack", "defense", "hp"]) words
+      best' = List.sort best
+  in case best' of
+    [] -> Epic.fail $ "no best IV given in appraisal"
+    _ ->
+      if best' /= List.nub best'
+        then Epic.fail $ "duplicate IV name in appraisal"
+        else return $ (best', rest)
+
+getBestRange :: (Epic.MonadCatch m) => [String] -> m (Range Int, [String])
+getBestRange words =
+  let
+    getRange phrase
+      | phrase `elem` ["not out", "point", "basic"]            = range 0 7
+      | phrase `elem` ["trending", "indicate", "definitely"]   = range 8 12
+      | phrase `elem` ["impressed", "excellent", "impressive"] = range 13 14
+      | phrase `elem` ["incredible", "blown", "no doubt"]      = range 15 15
+      | otherwise = Epic.fail $ "bad best IV description in appraisal"
+  in case words of
+       phrase : rest -> do
+         range <- getRange phrase
+         return $ (range, rest)
+       _ -> Epic.fail $ "missing best IV description in appraisal"
+
 ok :: Appraisal -> Int -> Int -> Int -> Bool
 ok this attack defense stamina =
   (attack + defense + stamina) `inRange` summary this &&
   checkBestRange this attack defense stamina &&
-  checkBestIvs this attack defense stamina
+  checkBest this attack defense stamina
 
 checkBestRange :: Appraisal -> Int -> Int -> Int -> Bool
 checkBestRange this attack defense stamina =
-  let bestValue = case head $ bestIvs this of
+  let bestValue = case head $ best this of
         "attack" -> attack
         "defense" -> defense
         "hp" -> stamina
   in bestValue `inRange` bestRange this
 
-checkBestIvs :: Appraisal -> Int -> Int -> Int -> Bool
-checkBestIvs this attack defense stamina =
-  case bestIvs this of
+checkBest :: Appraisal -> Int -> Int -> Int -> Bool
+checkBest this attack defense stamina =
+  case best this of
     ["attack"] ->
       attack > defense && attack > stamina
     ["attack", "defense"] ->
@@ -122,6 +109,9 @@ checkBestIvs this attack defense stamina =
       defense == stamina && defense > attack
     ["hp"] ->
       stamina > attack && stamina > defense
+
+split :: String -> String -> [String]
+split delim = Regex.splitRegex $ Regex.mkRegex delim
 
 -- inRange can be used sensibly as an infix operator.
 --
