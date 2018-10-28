f7579abf877224e24f4e4d9904a07f957b87ebed
Tom May <tom@tommay.net>
2018-10-28 02:00:29 -0700

    ...

--- app/PlotPowerups.hs
+++ app/PlotPowerups.hs
@@ -28,10 +28,13 @@ import           WeatherBonus (WeatherBonus)
 
 import qualified Debug
 
+import           Control.Applicative (optional, some, many)
 import           Control.Monad (join, mapM, forM_)
+import qualified Data.Attoparsec.Text as Atto
 import qualified Data.Char as Char
 import qualified Data.HashMap.Strict as HashMap
 import qualified Data.List as List
+import qualified Data.Text as Text
 import qualified System.Exit as Exit
 import qualified Text.Printf as Printf
 import qualified Text.Regex as Regex
@@ -41,6 +44,7 @@ data Options = Options {
   plotDps :: Bool,
   attackerSource :: AttackerSource,
   maybeEvolution :: Maybe String,
+  maybeMoveset   :: Maybe Moveset,
   defender :: Battler
 }
 
@@ -56,10 +60,12 @@ data Result = Result {
   tdo :: Int
 }
 
+type Moveset = (String, String)
+
 getOptions :: IO Options
 getOptions =
   let opts = Options <$> optPlotCandy <*> optPlotDps
-        <*> optAttackerSource <*> optEvolution <*> optDefender
+        <*> optAttackerSource <*> optEvolution <*> optMoveset <*> optDefender
       optPlotCandy =
         let optPlotCandy =
               O.flag' True
@@ -103,6 +109,11 @@ getOptions =
         <> O.short 'e'
         <> O.metavar "EVOLUTION"
         <> O.help "Final evolution desired")
+      optMoveset = O.optional $ (O.option $ optParseMoveset 0)
+        (  O.long "moveset"
+        <> O.short 'M'
+        <> O.metavar "ATTACKER_MOVESET"
+        <> O.help "Override the attacker moveset")
       optDefender = O.argument
         (BattlerUtil.optParseBattler IVs.defaultIVs)
         (O.metavar "DEFENDER[:LEVEL]")
@@ -112,6 +123,21 @@ getOptions =
              " vs. powerup candy"))
       prefs = O.prefs O.showHelpOnEmpty
   in O.customExecParser prefs options
+
+optParseMoveset :: a -> O.ReadM Moveset
+optParseMoveset = O.eitherReader . parseMoveset
+
+parseMoveset :: a -> String -> Either String Moveset
+parseMoveset _ string =
+  let attoParseMoveset = do
+        quickName <- many $ Atto.notChar '/'
+        Atto.char '/'
+        chargeName <- many $ Atto.anyChar
+        Atto.endOfInput
+        return (quickName, chargeName)
+  in case Atto.parseOnly attoParseMoveset (Text.pack string) of
+    Left _ -> Left $ "`" ++ string ++ "' should look like FAST/CHARGE"
+    Right moveset -> Right moveset
 
 main =
   Epic.catch (
