0444feb91a9ef80ce32fe1b76db2617418c02cdc
Tom May <tom@tommay.net>
2018-11-18 18:32:54 -0800

    Revert to c279bc395b637839cefb4ca181c5dfc2479b756d.  The ZeChrales GAME_MASTER had many things left as numbers instead of strings.

--- gamemaster.sh
+++ gamemaster.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# curl -O https://raw.githubusercontent.com/pokemongo-dev-contrib/pokemongo-game-master/master/versions/latest/GAME_MASTER.json
-
-curl https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/gamemaster/gamemaster.json >GAME_MASTER.json
+curl -O https://raw.githubusercontent.com/pokemongo-dev-contrib/pokemongo-game-master/master/versions/latest/GAME_MASTER.json
 
 json2yaml.rb GAME_MASTER.json >GAME_MASTER.yaml
 
