8ca591ec0f67dd5638d6f097d03a4220549d337d
Tom May <tom@tommay.net>
2018-12-20 11:09:16 -0800

    Attack takes a list of level and attackIv and prints a sorted list of absolute attack.

--- app/Attack.hs
+++ app/Attack.hs
@@ -8,29 +8,32 @@ import           Data.Semigroup ((<>))
 
 import qualified Epic
 import qualified GameMaster
+import           GameMaster (GameMaster)
 import qualified PokemonBase
-import qualified Type
+import           PokemonBase (PokemonBase)
+import qualified PokeUtil
 
 import           Control.Monad (join, forM_)
+import qualified Data.List as List
 import qualified System.Exit as Exit
 import qualified Text.Printf as Printf
 
 data Options = Options {
   species :: String,
-  level :: Float,
-  attack :: Int
+  stats :: [(Float, Int)]
 }
 
 getOptions :: IO Options
 getOptions =
-  let opts = Options <$> optSpecies <*> optLevel <*> optAttack
+  let opts = Options <$> optSpecies <*> optStats
       optSpecies = O.strArgument (O.metavar "SPECIES")
+      optStats = O.some $ (,) <$> optLevel <*> optAttack
       optLevel = O.argument O.auto (O.metavar "LEVEL")
       optAttack = O.argument O.auto (O.metavar "ATTACK")
       options = O.info (opts <**> O.helper)
         (  O.fullDesc
         <> O.progDesc
-             "Show how effective (or not) move types are against a pokemon")
+             "Show a species' absolute attack for different levels/attackIvs")
       prefs = O.prefs O.showHelpOnEmpty
   in O.customExecParser prefs options
 
@@ -39,11 +42,19 @@ main =
     do
       options <- getOptions
       gameMaster <- join $ GameMaster.load "GAME_MASTER.yaml"
-      base <- GameMaster.getPokemonBase gameMaster $ species options
-      let cpMultiplier = GameMaster.getCpMultiplier gameMaster (level options)
-          attackIv = attack options
-          absoluteAttack =
-            (fromIntegral $ PokemonBase.attack base + attackIv) * cpMultiplier
-      putStrLn $ show absoluteAttack
+      base <- GameMaster.getPokemonBase gameMaster (species options)
+      let augmented = augment (getAttack gameMaster base) (stats options)
+          sorted = List.reverse $ List.sortOn snd augmented
+      forM_ sorted $ \ ((level, attackIv), attack) ->
+        Printf.printf "%s %d: %f\n"
+          (PokeUtil.levelToString level) attackIv attack
     )
     $ Exit.die
+
+getAttack :: GameMaster -> PokemonBase -> (Float, Int) -> Float
+getAttack gameMaster base (level, attackIv) =
+  let cpMultiplier = GameMaster.getCpMultiplier gameMaster level
+  in (fromIntegral $ PokemonBase.attack base + attackIv) * cpMultiplier
+
+augment :: (a -> b) -> [a] -> [(a, b)]
+augment fn list = zip list (map fn list)
