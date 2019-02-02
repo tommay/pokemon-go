b9f84c5474762620f845c07838d76380053a11cb
Tom May <tom@tommay.net>
2019-02-02 13:05:22 -0800

    gamemaster,sh no longer needs to apply legacy_moves.patch.

--- gamemaster.sh
+++ gamemaster.sh
@@ -3,6 +3,3 @@
 curl -O https://raw.githubusercontent.com/pokemongo-dev-contrib/pokemongo-game-master/master/versions/latest/GAME_MASTER.json
 
 json2yaml.rb GAME_MASTER.json >GAME_MASTER.yaml
-
-patch --backup <legacy_moves.patch && \
-  diff -u GAME_MASTER.yaml.orig GAME_MASTER.yaml >legacy_moves.patch
