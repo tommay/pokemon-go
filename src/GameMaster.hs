c98116a285a2e2a92201c70bc07815ae9aedafaf
Tom May <tom@tommay.net>
2019-12-01 00:46:12 -0800

    Include shadow and purified pokemon in GameMaster's pokemonBases because Bulk will need them soon for determining costs.

--- src/GameMaster.hs
+++ src/GameMaster.hs
@@ -177,9 +177,7 @@ makeGameMaster yamlObject = do
   pokemonBases <-
     makeObjects "pokemonSettings" (getSpeciesForPokemonBase forms)
       (makePokemonBase types moves forms)
-      (filter (hasFormIfRequired forms) $
-       filter (not . isShadow) $
-       filter (not . isPurified) itemTemplates)
+      (filter (hasFormIfRequired forms) itemTemplates)
   cpMultipliers <- do
     playerLevel <- getFirst itemTemplates "playerLevel"
     getObjectValue playerLevel "cpMultiplier"
@@ -444,20 +442,6 @@ hasFormIfRequired forms itemTemplate =
                  -- it is simpler to annotate form but ignore the value like
                  -- this:
                  Right form -> const True (form :: String)
-
-isShadow :: ItemTemplate -> Bool
-isShadow = isForm "SHADOW"
-
-isPurified :: ItemTemplate -> Bool
-isPurified = isForm "PURIFIED"
-
-isForm :: String -> ItemTemplate -> Bool
-isForm form itemTemplate =
-  checkPokemonSettings itemTemplate $ \ pokemonSettings ->
-    case getObjectValue pokemonSettings "form" of
-      Left _ -> False
-      Right pokemonForm ->
-        ("_" ++ form) `List.isSuffixOf` pokemonForm
 
 -- This seems roundabout, but the good thing is that the type "a" is
 -- inferred from the usage context so the result is error-checked.
