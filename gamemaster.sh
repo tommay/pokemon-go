10f51430223d8ba148abc845c391681d105491d8
Tom May <tom@tommay.net>
2020-02-20 00:05:19 -0800

    Add argument to fetch from different branches, useful when there are updates in pull requests on another branch.

--- gamemaster.sh
+++ gamemaster.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-curl -O https://raw.githubusercontent.com/pokemongo-dev-contrib/pokemongo-game-master/master/versions/latest/GAME_MASTER.json
+if [ "$1" == "" ]; then
+  branch=master
+else
+  branch="$1"
+fi
+
+curl -O https://raw.githubusercontent.com/pokemongo-dev-contrib/pokemongo-game-master/$branch/versions/latest/GAME_MASTER.json
 
 json2yaml.rb GAME_MASTER.json >GAME_MASTER.yaml
