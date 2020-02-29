41e96f54fe154c5d160ec849cf73d3938cf0c96b
Tom May <tom@tommay.net>
2020-02-29 10:55:28 -0800

    Remove GAME_MASTER.json after converting to yaml.

--- gamemaster.sh
+++ gamemaster.sh
@@ -8,4 +8,4 @@ fi
 
 curl -O https://raw.githubusercontent.com/pokemongo-dev-contrib/pokemongo-game-master/$branch/versions/latest/GAME_MASTER.json
 
-json2yaml.rb GAME_MASTER.json >GAME_MASTER.yaml
+json2yaml.rb GAME_MASTER.json >GAME_MASTER.yaml && rm GAME_MASTER.json
