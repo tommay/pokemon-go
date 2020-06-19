2bc383f6d483ee3d7451a2113d160785d8300804
Tom May <tom@tommay.net>
2020-06-19 12:02:55 -0700

    rate.sh allows the inout file to include a species name.

--- rate.sh
+++ rate.sh
@@ -9,15 +9,23 @@
 # "best [-a] species.team.*" to sort by stat product or attack and
 # decide which pokemon to keep.
 
-species=${1%%.*}           # Extract justthe species from species.team.
+species=${1%%.*}           # Extract just the species from species.team.
 species=${species##*/}     # Remove any directory.
 
 while read stats; do
-  ./bulk -s -g $species $stats
+  # If the stats line starts with a letter then it already includes
+  # the species, else prepend the species.
+  if [[ "$stats" != [a-z]* ]]; then
+    stats="$species $stats"
+  fi
+  ./bulk -s -g $stats
 done <$1 | egrep -v "too high" >$1.great
 
 while read stats; do
-  ./bulk -s -u $species $stats
+  if [[ "$stats" != [a-z]* ]]; then
+    stats="$species $stats"
+  fi
+  ./bulk -s -u $stats
 done <$1 | egrep -v "too high" >$1.ultra
 
 
