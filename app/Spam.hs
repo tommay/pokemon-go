a0951f8fc614ee939a0e151b1536edde0bb866d0
Tom May <tom@tommay.net>
2021-07-03 10:25:08 -0700

    spam wip.

--- app/Spam.hs
+++ app/Spam.hs
@@ -9,9 +9,12 @@ import qualified Data.Attoparsec.Text as Atto
 import           Data.Semigroup ((<>))
 import qualified Data.Text as Text
 
+import qualified Calc
 import qualified Epic
 import qualified GameMaster
 import           GameMaster (GameMaster)
+import qualified IVs
+import           IVs (IVs)
 import qualified Move
 import           Move (Move)
 import qualified Pokemon
@@ -20,6 +23,7 @@ import qualified PokemonBase
 import           PokemonBase (PokemonBase)
 import qualified Type
 import           Type (Type)
+import qualified Util
 
 import qualified Debug
 
@@ -30,14 +34,20 @@ import qualified Text.Printf as Printf
 
 type Battler = (String, (Int, Int, Int))
 
+data League = Little | Great | Ultra | Master | Peewee
+  deriving (Eq, Show)
+
 data Options = Options {
   attacker :: Battler,
-  defender :: Maybe String
+  defender :: Maybe String,
+  league :: League,
+  useStatProduct :: Bool
 }
 
 getOptions :: GameMaster -> IO Options
 getOptions gameMaster =
-  let opts = Options <$> optAttacker <*> optDefender
+  let opts = Options <$> optAttacker <*> optDefender <*> optLeague <*>
+        optUseStatProduct
       allSpecies = GameMaster.allSpecies gameMaster
       optAttacker = O.argument
         optParseBattler
@@ -45,11 +55,47 @@ getOptions gameMaster =
       optDefender = O.optional $ O.strArgument
         (  (O.metavar "DEFENDER")
         <> O.completeWith allSpecies)
+      optLeague =
+            O.flag' Little (
+              -- There is no short option because the obvious "-l" conflicts
+              -- with the short option for "--level".
+              O.long "little" <>
+              O.help "little league")
+        <|> O.flag' Great (
+              O.short 'g' <>
+              O.long "great" <>
+              O.help "great league")
+        <|> O.flag' Ultra (
+              O.short 'u' <>
+              O.long "ultra" <>
+              O.help "ultra league")
+        <|> O.flag' Master (
+              O.short 'm' <>
+              O.long "master" <>
+              O.help "master league")
+        <|> O.flag' Peewee (
+              O.short 'p' <>
+              O.long "peewee" <>
+              O.help "peewee league")
+        <|> pure Great
+      optUseStatProduct = O.switch (
+        O.short 's' <>
+        O.long "statproduct" <>
+        O.help "use statproduct when calculating damage")
       options = O.info (opts <**> O.helper)
         (  O.fullDesc
         <> O.progDesc "Rate movesets by spamminess.")
       prefs = O.prefs O.showHelpOnEmpty
   in O.customExecParser prefs options
+
+leaguePred :: League -> (Int -> Bool)
+leaguePred league =
+  case league of
+    Little -> (<= 500)
+    Great -> (<= 1500)
+    Ultra -> (<= 2500)
+    Master -> const True
+    Peewee -> (<= 10)
 
 defaultIVs = (4, 13, 13)
 
@@ -60,6 +106,10 @@ main =
       options <- getOptions gameMaster
       let (attacker, ivs) = Main.attacker options
       base <- GameMaster.getPokemonBase gameMaster $ attacker
+      let statProduct = if useStatProduct options
+            then getPoweredUpStatProduct gameMaster base
+              (leaguePred $ league options) ivs / 1000000
+            else 1
       maybeDefenderBase <- do
         case defender options of
           Just species -> Just <$> GameMaster.getPokemonBase gameMaster species
@@ -88,13 +138,40 @@ main =
             let name = toName fast charged
                 (turnsPerCycle, fastMovesPerCycle, dpt, dpe) =
                   spam types defenderTypes fast charged
-            in (name, turnsPerCycle, fastMovesPerCycle, dpt, dpe)
+            in (name, turnsPerCycle, fastMovesPerCycle, dpt * statProduct, dpe)
           stuff = map toStuff moveSets
       forM_ stuff $ \ (name, turnsPerCycle, fastMovesPerCycle, dpt, dpe) ->
         putStrLn $ Printf.printf "%-*s: %d(%d) %.2f %.2f"
           maxNameLength name turnsPerCycle fastMovesPerCycle dpt dpe
     )
     $ Exit.die
+
+allIvs =
+  let ivs = [0..15]
+  in [(attack, defense, stamina) |
+       attack <- ivs, defense <- ivs, stamina <- ivs]
+
+getPoweredUpStatProduct :: GameMaster -> PokemonBase -> (Int -> Bool) -> (Int, Int, Int) -> Float
+getPoweredUpStatProduct gameMaster base leaguePred (attack, defense, stamina) =
+  getStatProduct gameMaster base $
+    getPoweredUpIVs gameMaster base leaguePred (attack, defense, stamina)
+
+getPoweredUpIVs :: GameMaster -> PokemonBase -> (Int -> Bool) -> (Int, Int, Int) -> IVs
+getPoweredUpIVs gameMaster base leaguePred (attack, defense, stamina) =
+  Util.lastWhere (leaguePred . Calc.cp gameMaster base) $
+    map (\ level -> IVs.new level attack defense stamina) $
+    GameMaster.powerUpLevels gameMaster
+
+getStatProduct :: GameMaster -> PokemonBase -> IVs -> Float
+getStatProduct gameMaster base ivs =
+  let cpMultiplier = GameMaster.getCpMultiplier gameMaster $ IVs.level ivs
+  in foldr (*) 1 $
+       map (* cpMultiplier) $
+       map fromIntegral $
+       map (\ (a, b) -> a base + b ivs)
+         [(PokemonBase.attack, IVs.attack),
+          (PokemonBase.defense, IVs.defense),
+          (PokemonBase.stamina, IVs.stamina)]
 
 spam :: [Type] -> [Type] -> Move -> Move -> (Int, Int, Float, Float)
 spam types defenderTypes fast charged =
--- spamminess.sh
+++ spamminess.sh
@@ -2,7 +2,8 @@
 
 # Sorts all pokemon+moveset first by spamminess (how many turns it
 # takes for them to do a charge move, lowest first) and secondarily by
-# how much damage they do adjusted for the moves' STAB.
+# how much damage they do adjusted for the moves' STAB and the attacker's
+# stat product.
 #
 # The idea is that spammy moves are good because it's not a big deal
 # if they get shielded, but they also need to put out respectable
@@ -12,7 +13,7 @@
 
 all_pokemon=$(./list "$@")
 for p in $all_pokemon; do
-  ./spam $p |
+  ./spam $p --little -s |
     awk -F: '{ print $2, $1 }' |
     sed -e "s/^ *//" -e "s/ *$/ $p/"
 done | sort -k 1n -k 2nr
