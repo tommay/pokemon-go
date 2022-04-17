454ff917e314b7b895bb9770ed04e6dd09909551
Tom May <tom@tommay.net>
2022-04-16 23:36:28 -0700

    Add Debug.traceWith.

--- src/Debug.hs
+++ src/Debug.hs
@@ -1,5 +1,6 @@
 module Debug (
-  spew
+  spew,
+  spewWith
 ) where
 
 import qualified Debug.Trace as Trace
@@ -7,3 +8,7 @@ import qualified Debug.Trace as Trace
 spew :: (Show a, Show b) => a -> b -> b 
 spew a b =
   Trace.traceShow (a, b) b
+
+spewWith :: (Show a, Show c) => (b -> c) -> a -> b -> b
+spewWith fn a b =
+  Trace.traceShow (a, fn b) b
