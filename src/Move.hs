df4479c93dea6dfbb306f3159f9cc1282afa81f5
Tom May <tom@tommay.net>
2023-08-29 20:51:41 -0700

    Derive Move.name from Move.vfxName.

--- src/Move.hs
+++ src/Move.hs
@@ -81,7 +81,7 @@ durationMs this =
 
 name :: Move -> String
 name this =
-  let noFast = Regex.subRegex (Regex.mkRegex "_FAST$") (movementId this) ""
+  let noFast = Regex.subRegex (Regex.mkRegex "_fast$") (vfxName this) ""
       alphaOnly = Regex.subRegex (Regex.mkRegex "[^a-zA-Z]") noFast " "
       withType = if Move.isHiddenPower this
         then alphaOnly ++ ", " ++ (Type.name $ Move.moveType this)
